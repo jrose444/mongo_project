f5417eddae2e55d2fbebd08f8ec948b4d335bcda
Jeremy <Jrmyrosenberg@gmail.com>
2019-11-17 13:18:45 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB successfully"))
+  .catch(err => console.log(err));
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Hello Gorld"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
